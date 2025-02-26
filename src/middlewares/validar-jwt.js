e1c6db10bfbbdab07f1ba0f29fbd04edada25f28
Jeremy <jarevalo-2023528@kinal.edu.gt>
2025-02-26 03:23:31 -0600

    connection

---
+++ src/middlewares/validar-jwt.js
@@ -0,0 +1,50 @@
+import jwt from 'jsonwebtoken';
+
+import Usuario from '../users/user.model.js';
+
+export const validarJWT = async(req, res, next) =>{
+    
+    const token = req.header('x-token');
+
+    if (!token) {
+        return res.status(401).json({
+            msg: 'No hay token en la peticion'
+        })
+    }
+
+
+
+    try {
+        const {uid} = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+        const usuario = await Usuario.findById(uid);
+
+        if (!usuario) {
+            return res.status(401).json({
+                msg: 'Usuario no existe en la base de datos'
+            })
+        }
+
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Token no valido - usuario conn estado: false'
+            })
+        };
+
+
+        if (usuario.role !== 'ADMIN_ROLE') {
+            return res.status(403).json({
+                msg: 'No tienes permisos para realizar esta acción'
+            });
+        }
+
+        req.usuario = usuario;
+
+        next();
+    } catch (e) {
+        console.log(e);
+        res.status(401).json({
+            msg: 'Token no valido'
+        })
+    }
+
+}
